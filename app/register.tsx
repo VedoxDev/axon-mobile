716fa75c0767b48e92c64b5486f8cb19375cefa3
VedoxDev <v.a.fonseca.y@gmail.com>
2025-06-01 20:45:32 +0200

    fix(register): Añadida validación antes de permitir el envio de request para registrarse

--- app/register.tsx
+++ app/register.tsx
@@ -1,7 +1,7 @@
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { Feather } from '@expo/vector-icons';
 import { Stack, router } from "expo-router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ActivityIndicator, Alert, Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Colors } from '../constants/Colors';
@@ -30,12 +30,53 @@ export default function RegisterScreen() {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
 
+  // Keyboard state for Android
+  const [keyboardVisible, setKeyboardVisible] = useState(false);
+
   const [passwordCriteria, setPasswordCriteria] = useState({
     length: false,
     uppercase: false,
     number: false,
     symbol: false,
   });
+
+  // Keyboard event listeners for Android
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
+        setKeyboardVisible(true);
+      });
+      const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => {
+        setKeyboardVisible(false);
+      });
+
+      return () => {
+        keyboardDidShowListener?.remove();
+        keyboardDidHideListener?.remove();
+      };
+    }
+  }, []);
+
+  // Function to filter numbers and special characters from name fields
+  const filterNumbers = (text: string) => {
+    // Remove numbers and special characters, only allow letters, spaces, hyphens, and accented characters
+    return text.replace(/[^a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s\-']/g, '');
+  };
+
+  // Email validation function
+  const isValidEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email.trim());
+  };
+
+  // Check if form is valid
+  const isFormValid = 
+    nombre.trim().length > 0 && 
+    apellidos.trim().length > 0 && 
+    isValidEmail(email) && 
+    password.length > 0 && 
+    confirmPassword.length > 0 && 
+    termsAccepted;
 
   const validatePassword = (text: string) => {
     const criteria = {
@@ -72,8 +113,7 @@ export default function RegisterScreen() {
     }
 
     // Email validation
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email.trim())) {
+    if (!isValidEmail(email.trim())) {
       Alert.alert('Error', 'Por favor ingresa un email válido');
       return;
     }
@@ -108,168 +148,180 @@ export default function RegisterScreen() {
   const insets = useSafeAreaInsets();
 
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={styles.keyboardAvoidingContainer}
-      >
-        <View style={[styles.container, { backgroundColor: theme.background }]}>
-          <Stack.Screen options={{ headerShown: false }} />
-          {/* Title and Subtitle */}
-          <Text style={[styles.title, { color: theme.text, textAlign: 'left', fontFamily: 'Inter-Bold' }]}>Registrate</Text>
-          <Text style={[styles.subtitle, { color: theme.text, textAlign: 'left', fontFamily: 'Inter-Regular' }]}>Crea tu cuenta para continuar</Text>
+    <View style={[styles.fullContainer, { backgroundColor: theme.background }]}>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : Platform.OS === 'android' ? 'height' : undefined}
+          style={[styles.keyboardAvoidingContainer, { backgroundColor: theme.background }]}
+          keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 0}
+        >
+          <View style={[styles.container, { 
+            backgroundColor: theme.background,
+            paddingBottom: Platform.OS === 'android' && keyboardVisible ? 0 : 20 
+          }]}>
+            <Stack.Screen options={{ headerShown: false }} />
+            {/* Title and Subtitle */}
+            <Text style={[styles.title, { color: theme.text, textAlign: 'left', fontFamily: 'Inter-Bold' }]}>Registrate</Text>
+            <Text style={[styles.subtitle, { color: theme.text, textAlign: 'left', fontFamily: 'Inter-Regular' }]}>Crea tu cuenta para continuar</Text>
 
-          {/* Input Fields */}
-          <TextInput
-            style={[styles.input, { backgroundColor: colorScheme === 'dark' ? theme.inputBackground : theme.card, color: theme.text, borderColor: nombreFocused ? theme.orange : theme.gray }]}
-            placeholder="Nombre"
-            placeholderTextColor={theme.text + "80"}
-            value={nombre}
-            onChangeText={setNombre}
-            onFocus={() => setNombreFocused(true)}
-            onBlur={() => setNombreFocused(false)}
-            autoCapitalize="words"
-            editable={!isLoading}
-          />
-
-          <TextInput
-            style={[styles.input, { backgroundColor: colorScheme === 'dark' ? theme.inputBackground : theme.card, color: theme.text, borderColor: apellidosFocused ? theme.orange : theme.gray }]}
-            placeholder="Apellidos"
-            placeholderTextColor={theme.text + "80"}
-            value={apellidos}
-            onChangeText={setApellidos}
-            onFocus={() => setApellidosFocused(true)}
-            onBlur={() => setApellidosFocused(false)}
-            autoCapitalize="words"
-            editable={!isLoading}
-          />
-
-          <View style={[styles.inputContainer, { borderColor: correoFocused ? theme.orange : theme.gray, backgroundColor: colorScheme === 'dark' ? theme.inputBackground : theme.card }]}>
+            {/* Input Fields */}
             <TextInput
-              style={[styles.inputInsideContainer, { color: theme.text }]}
-              placeholder="Correo"
+              style={[styles.input, { backgroundColor: colorScheme === 'dark' ? theme.inputBackground : theme.card, color: theme.text, borderColor: nombreFocused ? theme.orange : theme.gray }]}
+              placeholder="Nombre"
               placeholderTextColor={theme.text + "80"}
-              value={email}
-              onChangeText={setEmail}
-              onFocus={() => setCorreoFocused(true)}
-              onBlur={() => setCorreoFocused(false)}
-              keyboardType="email-address"
-              autoCapitalize="none"
-              autoCorrect={false}
+              value={nombre}
+              onChangeText={(text) => setNombre(filterNumbers(text))}
+              onFocus={() => setNombreFocused(true)}
+              onBlur={() => setNombreFocused(false)}
+              autoCapitalize="words"
               editable={!isLoading}
             />
-          </View>
 
-          {/* Password Input and Strength Box Container */}
-          <View style={styles.passwordInputWrapper}>
-            <View style={[styles.inputContainer, {
-              borderColor: !passwordFocused && password.length > 0 && (!passwordCriteria.length || !passwordCriteria.uppercase || !passwordCriteria.number || !passwordCriteria.symbol) ? theme.red : passwordFocused ? theme.orange : theme.gray,
-              backgroundColor: colorScheme === 'dark' ? theme.inputBackground : theme.card
-            }]}>
+            <TextInput
+              style={[styles.input, { backgroundColor: colorScheme === 'dark' ? theme.inputBackground : theme.card, color: theme.text, borderColor: apellidosFocused ? theme.orange : theme.gray }]}
+              placeholder="Apellidos"
+              placeholderTextColor={theme.text + "80"}
+              value={apellidos}
+              onChangeText={(text) => setApellidos(filterNumbers(text))}
+              onFocus={() => setApellidosFocused(true)}
+              onBlur={() => setApellidosFocused(false)}
+              autoCapitalize="words"
+              editable={!isLoading}
+            />
+
+            <View style={[styles.inputContainer, { borderColor: correoFocused ? theme.orange : theme.gray, backgroundColor: colorScheme === 'dark' ? theme.inputBackground : theme.card }]}>
               <TextInput
                 style={[styles.inputInsideContainer, { color: theme.text }]}
-                placeholder="Contraseña"
+                placeholder="Correo"
                 placeholderTextColor={theme.text + "80"}
-                value={password}
-                secureTextEntry={!isPasswordVisible}
-                onFocus={() => setPasswordFocused(true)}
-                onBlur={() => setPasswordFocused(false)}
-                onChangeText={validatePassword}
+                value={email}
+                onChangeText={setEmail}
+                onFocus={() => setCorreoFocused(true)}
+                onBlur={() => setCorreoFocused(false)}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                editable={!isLoading}
+              />
+            </View>
+
+            {/* Password Input and Strength Box Container */}
+            <View style={styles.passwordInputWrapper}>
+              <View style={[styles.inputContainer, {
+                borderColor: !passwordFocused && password.length > 0 && (!passwordCriteria.length || !passwordCriteria.uppercase || !passwordCriteria.number || !passwordCriteria.symbol) ? theme.red : passwordFocused ? theme.orange : theme.gray,
+                backgroundColor: colorScheme === 'dark' ? theme.inputBackground : theme.card
+              }]}>
+                <TextInput
+                  style={[styles.inputInsideContainer, { color: theme.text }]}
+                  placeholder="Contraseña"
+                  placeholderTextColor={theme.text + "80"}
+                  value={password}
+                  secureTextEntry={!isPasswordVisible}
+                  onFocus={() => setPasswordFocused(true)}
+                  onBlur={() => setPasswordFocused(false)}
+                  onChangeText={validatePassword}
+                  editable={!isLoading}
+                />
+                <TouchableOpacity
+                  style={styles.passwordVisibilityToggle}
+                  onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+                  disabled={isLoading}
+                >
+                  <Feather
+                    name={isPasswordVisible ? 'eye-off' : 'eye'}
+                    size={20}
+                    color={theme.text + '80'}
+                  />
+                </TouchableOpacity>
+              </View>
+
+              {passwordFocused && password.length > 0 && (
+                <PasswordStrengthBox criteria={passwordCriteria} theme={theme} />
+              )}
+            </View>
+
+            <View style={[styles.inputContainer, { borderColor: confirmPasswordFocused ? theme.orange : theme.gray, backgroundColor: colorScheme === 'dark' ? theme.inputBackground : theme.card }]}>
+              <TextInput
+                style={[styles.inputInsideContainer, { color: theme.text }]}
+                placeholder="Confirmar contraseña"
+                placeholderTextColor={theme.text + "80"}
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+                secureTextEntry={!isConfirmPasswordVisible}
+                onFocus={() => setConfirmPasswordFocused(true)}
+                onBlur={() => setConfirmPasswordFocused(false)}
                 editable={!isLoading}
               />
               <TouchableOpacity
                 style={styles.passwordVisibilityToggle}
-                onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+                onPress={() => setIsConfirmPasswordVisible(!isConfirmPasswordVisible)}
                 disabled={isLoading}
               >
                 <Feather
-                  name={isPasswordVisible ? 'eye-off' : 'eye'}
+                  name={isConfirmPasswordVisible ? 'eye-off' : 'eye'}
                   size={20}
                   color={theme.text + '80'}
                 />
               </TouchableOpacity>
             </View>
 
-            {passwordFocused && password.length > 0 && (
-              <PasswordStrengthBox criteria={passwordCriteria} theme={theme} />
-            )}
-          </View>
+            {/* Checkbox */}
+            <View style={styles.checkboxContainer}>
+              <TouchableOpacity 
+                style={[styles.checkbox, { borderColor: termsAccepted ? theme.orange : theme.gray, backgroundColor: termsAccepted ? theme.orange : 'transparent' }]} 
+                onPress={() => setTermsAccepted(!termsAccepted)}
+                disabled={isLoading}
+              >
+                {termsAccepted && <Feather name="check" size={16} color="white" />}
+              </TouchableOpacity>
+              <Text style={[styles.checkboxLabel, { color: theme.text, fontFamily: 'Inter-Regular' }]}>Acepto los Términos y Condiciones y la Política de Privacidad</Text>
+            </View>
 
-          <View style={[styles.inputContainer, { borderColor: confirmPasswordFocused ? theme.orange : theme.gray, backgroundColor: colorScheme === 'dark' ? theme.inputBackground : theme.card }]}>
-            <TextInput
-              style={[styles.inputInsideContainer, { color: theme.text }]}
-              placeholder="Confirmar contraseña"
-              placeholderTextColor={theme.text + "80"}
-              value={confirmPassword}
-              onChangeText={setConfirmPassword}
-              secureTextEntry={!isConfirmPasswordVisible}
-              onFocus={() => setConfirmPasswordFocused(true)}
-              onBlur={() => setConfirmPasswordFocused(false)}
-              editable={!isLoading}
-            />
-            <TouchableOpacity
-              style={styles.passwordVisibilityToggle}
-              onPress={() => setIsConfirmPasswordVisible(!isConfirmPasswordVisible)}
-              disabled={isLoading}
-            >
-              <Feather
-                name={isConfirmPasswordVisible ? 'eye-off' : 'eye'}
-                size={20}
-                color={theme.text + '80'}
-              />
-            </TouchableOpacity>
-          </View>
-
-          {/* Checkbox */}
-          <View style={styles.checkboxContainer}>
+            {/* Register Button */}
             <TouchableOpacity 
-              style={[styles.checkbox, { borderColor: termsAccepted ? theme.orange : theme.gray, backgroundColor: termsAccepted ? theme.orange : 'transparent' }]} 
-              onPress={() => setTermsAccepted(!termsAccepted)}
-              disabled={isLoading}
+              style={[styles.registerButton, { 
+                backgroundColor: isLoading ? theme.gray : theme.primary,
+                opacity: (isFormValid && !isLoading) ? 1 : 0.4
+              }]} 
+              onPress={handleRegister}
+              disabled={isLoading || !isFormValid}
             >
-              {termsAccepted && <Feather name="check" size={16} color="white" />}
+              {isLoading ? (
+                <ActivityIndicator size="small" color="white" />
+              ) : (
+                <Text style={[styles.registerButtonText, { fontFamily: 'Inter-Bold' }]}>Registrarse</Text>
+              )}
             </TouchableOpacity>
-            <Text style={[styles.checkboxLabel, { color: theme.text, fontFamily: 'Inter-Regular' }]}>Acepto los Términos y Condiciones y la Política de Privacidad</Text>
-          </View>
 
-          {/* Register Button */}
-          <TouchableOpacity 
-            style={[styles.registerButton, { backgroundColor: isLoading ? theme.gray : theme.primary }]} 
-            onPress={handleRegister}
-            disabled={isLoading}
-          >
-            {isLoading ? (
-              <ActivityIndicator size="small" color="white" />
-            ) : (
-              <Text style={[styles.registerButtonText, { fontFamily: 'Inter-Bold' }]}>Registrarse</Text>
-            )}
-          </TouchableOpacity>
-
-          {/* Login Link */}
-          <View style={[{ flexDirection: 'row', justifyContent: 'center', width: '100%', marginTop: 20, alignItems: 'baseline' }]}>
-            <Text style={[{ color: theme.text, fontFamily: 'Inter-Regular', fontSize: 14 }]}>
-              ¿Ya tienes una cuenta?
-            </Text>
-            <TouchableOpacity onPress={() => router.replace('/login')} style={{ marginLeft: 4 }} disabled={isLoading}>
-              <Text style={{ color: theme.orange, fontFamily: 'Inter-Bold', fontSize: 14 }}>
-                Inicia sesión
+            {/* Login Link */}
+            <View style={[{ flexDirection: 'row', justifyContent: 'center', width: '100%', marginTop: 20, alignItems: 'baseline' }]}>
+              <Text style={[{ color: theme.text, fontFamily: 'Inter-Regular', fontSize: 14 }]}>
+                ¿Ya tienes una cuenta?
               </Text>
-            </TouchableOpacity>
-          </View>
+              <TouchableOpacity onPress={() => router.replace('/login')} style={{ marginLeft: 4 }} disabled={isLoading}>
+                <Text style={{ color: theme.orange, fontFamily: 'Inter-Bold', fontSize: 14 }}>
+                  Inicia sesión
+                </Text>
+              </TouchableOpacity>
+            </View>
 
-        </View>
-      </KeyboardAvoidingView>
-    </TouchableWithoutFeedback>
+          </View>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  fullContainer: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
     padding: 20,
-    marginTop: 0, // Adjusted for register screen
+    marginTop: 0,
   },
   keyboardAvoidingContainer: {
     flex: 1,
